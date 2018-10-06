6cc0b89af1fa9001b4f4513fe218e91c1247782c
George Mitchell <georgemaxwellmitchell@outlook.com>
2018-10-06 14:13:26 -0500

    renamed clarifai obj

--- clarifai.js
+++ clarifai.js
@@ -1,10 +1,11 @@
 const Clarifai = require('clarifai');
 
-const app = new Clarifai.App({
+const clarifaiApp = new Clarifai.App({
     apiKey: '468e1a63a329408a9d28686d14cf5efb'
 });
 
-const workflow = Workflow
+// need to add workflow?
+//const workflow = Workflow
 
 // Have already trained the model; need to retreive it
-const model = app.models.get('SoyBoy');
+const model = clarifaiApp.models.get('SoyBoy');
