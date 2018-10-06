b089242417535359378031bf2a5d55d75fa3482d
George Mitchell <georgemaxwellmitchell@outlook.com>
2018-10-06 14:17:47 -0500

    Added image prediction functions

--- clarifai.js
+++ clarifai.js
@@ -1,4 +1,5 @@
 const Clarifai = require('clarifai');
+const fs = require('fs');
 
 const clarifaiApp = new Clarifai.App({
     apiKey: '468e1a63a329408a9d28686d14cf5efb'
@@ -9,3 +10,5 @@ const clarifaiApp = new Clarifai.App({
 
 // Have already trained the model; need to retreive it
 const model = clarifaiApp.models.get('SoyBoy');
+const userImage = ClImage(open('testFile.jpg', 'rb'));
+model.predict([userImage]);
