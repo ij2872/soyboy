2baaf6966c3d1150b3d488381c80bb2c02f62d6f
George Mitchell <georgemaxwellmitchell@outlook.com>
2018-10-06 14:48:16 -0500

    Removed comment

--- clarifai.js
+++ clarifai.js
@@ -4,7 +4,6 @@ const clarifaiApp = new Clarifai.App({
     apiKey: '468e1a63a329408a9d28686d14cf5efb'
 });
 
-// Have already trained the model; need to retreive it
 const model = clarifaiApp.models.get('SoyBoy');
 
 function predictUserImage(userImagePath) {
