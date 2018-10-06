179213bb55223cbff1ce67114b54f36e1ada838c
George Mitchell <georgemaxwellmitchell@outlook.com>
2018-10-06 14:47:38 -0500

    added js function for prediction

--- clarifai.js
+++ clarifai.js
@@ -1,14 +1,13 @@
 const Clarifai = require('clarifai');
-const fs = require('fs');
 
 const clarifaiApp = new Clarifai.App({
     apiKey: '468e1a63a329408a9d28686d14cf5efb'
 });
 
-// need to add workflow?
-//const workflow = Workflow
-
 // Have already trained the model; need to retreive it
 const model = clarifaiApp.models.get('SoyBoy');
-const userImage = ClImage(open('res/testFile.jpg', 'rb'));
-model.predict([userImage]);
+
+function predictUserImage(userImagePath) {
+    let userImage = ClImage(open(userImagePath, 'rb'));
+    return model.predict([userImage]);
+}
