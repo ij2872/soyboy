e9b535ae1272089ca103ba0464dc8b111249f915
Ivan <ijaimes2872@gmail.com>
2018-10-06 14:50:27 -0500

    moved clarify to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+// Clarify
+const Clarifai = require('clarifai');
+
+const clarifaiApp = new Clarifai.App({
+    apiKey: '468e1a63a329408a9d28686d14cf5efb'
+});
+
+const model = clarifaiApp.models.get('SoyBoy');
+
+function predictUserImage(userImagePath) {
+    let userImage = ClImage(open(userImagePath, 'rb'));
+    return model.predict([userImage]);
+}
+
+//------
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,12 +32,24 @@ router.post('/', function(req, res){
 });
 
 router.get('/results:id', function(req,res){
+
+  var id = req.params.id;
+  var imgLocation = "./img/"
+  var soyPerc = 90;
+  var isSoy = (soyPerc > 80) ? true : false;
+  var isBeard = false;
+
   var jsonResults = {
     status: false,
-    imgLocation: './resc/img.jpg',
-    id: req.params.id
+    imgLocation: imgLocation,
+    id: id,
+    data: {
+      soyPercentage: soyPerc,
+      isSoy: isSoy,
+      isBeard: isBeard
+    }
   };
-  
+
   res.send(JSON.stringify(jsonResults));
 });
 
