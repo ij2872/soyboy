a43e853cb98694257d4f8a15d0d2912a20b27f4b
Ivan <ijaimes2872@gmail.com>
2018-10-06 14:37:11 -0500

    working on routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res){
+  res.send('It Worked!');
+});
+
+router.get('/results:id', function(req,res){
+  var jsonResults = {
+    status: false,
+    imgLocation: './resc/img.jpg',
+    id: req.params.id
+  };
+  
+  res.send(JSON.stringify(jsonResults));
+});
+
 module.exports = router;
